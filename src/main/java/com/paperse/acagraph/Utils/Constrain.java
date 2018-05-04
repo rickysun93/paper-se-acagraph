6f6590b1fb68dda856d5abe070d200a2a312349b
rickysun93 <rickysun93@outlook.com>
2018-05-04 15:35:31 +0800

    move to 3201

--- src/main/java/com/paperse/acagraph/Utils/Constrain.java
+++ src/main/java/com/paperse/acagraph/Utils/Constrain.java
@@ -7,6 +7,6 @@ public class Constrain {
     public static final String devinfilePath = "/Users/sunhaoran/paper_sun_shen.txt";
     public static final String devoutfilePath = "/Users/sunhaoran/data0.txt";
 
-    public static final String infilePath = "/root/paper_sun_shen.txt";
-    public static final String outfilePath = "/root/data0.txt";
+    public static final String infilePath = "/home/administrator/paper_sun_shen.txt";
+    public static final String outfilePath = "/home/administrator/data0.txt";
 }
