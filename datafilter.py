6c2634b57e24834e6a49c77d46afba9c51998594
System Administrator <administrator@s07.c1>
2018-05-23 10:23:16 +0800

    update filter

--- datafilter.py
+++ datafilter.py
@@ -2,18 +2,26 @@ import pymongo
 import json
 
 
-def datafilter(ip="localhost", port=27017, dbname="papertest"):
+def datafilter(ip="localhost", port=27017, dbname="papertest2"):
     conn = pymongo.MongoClient(ip, port)
     db = conn[dbname]
     mpaper = db.mPaper
+    ids = {}
+    i = 0
+    for line in mpaper.find():
+        ids[line['oriid']] = 1
+    print("ok")
     for line in mpaper.find():
         refs = json.loads(line['refs'])
         nrefs = []
         for ref in refs:
-            if mpaper.find_one({'oriid': ref}) is not None:
+            if ref in ids:
                 nrefs.append(ref)
         line['refs'] = json.dumps(nrefs)
-        mpaper.update_one({'oriid': line['oriid']}, {'$set': line})
+        mpaper.update_one({'_id': line['_id']}, {'$set': line})
+        i += 1
+        if i%1000 == 0:
+            print(str(i))
 
 
 def test():
